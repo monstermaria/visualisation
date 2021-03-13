3eb5277bebde0f6c69761859914812402d569a76
Maria Andersson <monstermaria@gmail.com>
2021-03-13 19:10:36 +0100

    Improved detection of files and added css classes and styling

--- src/FileTreeComponent.js
+++ src/FileTreeComponent.js
@@ -1,3 +1,4 @@
+import "./FileTreeComponent.css";
 import PropTypes from "prop-types";
 import React from "react";
 
@@ -7,30 +8,23 @@ function FileTreeComponent(props) {
   // create an array for storing the subfolder components
   const subFolders = [];
 
-  // assuming all indata represents a file, and not an empty folder
-  // "folders" without any subfolders is categorised as a file
-  let isFile = true;
-
   // loop through the properties (subfolders) of the sub tree prop
   for (const folderName in props.subTree) {
 
-    // this folder has subfolders, that means it is not a file
-    isFile = false;
+    // assuming all indata represents a file, and not an empty folder
+    // "folders" without any subfolders is categorised as a file
+    const isFile = Object.keys(props.subTree[folderName]).length === 0;
+
+    // decide class for this list item
+    const listItemClass = isFile ? "file" : "folder";
 
     // create a list item that contains a FileTreeComponent component
-    const folder = <li key={folderName}><FileTreeComponent name={folderName} subTree={props.subTree[folderName]} /></li>;
+    const folder = <li key={folderName} className={listItemClass}>
+      <FileTreeComponent name={folderName} subTree={props.subTree[folderName]} />
+    </li>;
 
     // add the list item to the list of sub folders
     subFolders.push(folder);
-  }
-
-  // a file has been detected and is rendered in another way than a folder
-  if (isFile) {
-    return (
-      <React.Fragment>
-        {props.name + " (file)"}
-      </React.Fragment>
-    );
   }
 
   // render the name of the folder and its contents
