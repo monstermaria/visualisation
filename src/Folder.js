980f809161de2ec939b2f45d9e25631f55cac2d9
Maria Andersson <monstermaria@gmail.com>
2021-03-13 13:59:01 +0100

    Added more comments for clarity

--- src/Folder.js
+++ src/Folder.js
@@ -6,11 +6,17 @@ function Folder(props) {
   // create an array for storing the sub folder components
   const subFolders = [];
 
+  // loop through the properties (sub folders) of the sub tree prop
   for (const folderName in props.subTree) {
+
+    // create a list item that contains a Folder component
     const folder = <li key={folderName}><Folder name={folderName} subTree={props.subTree[folderName]} /></li>;
+
+    // add the list item to the list of sub folders
     subFolders.push(folder);
   }
 
+  // render the name of the folder and its contents
   return (
     <div>
       {props.name}
@@ -19,6 +25,7 @@ function Folder(props) {
   );
 }
 
+// using prop types to get a warning if the component don't get the right props
 Folder.propTypes = {
   name: PropTypes.string.isRequired,
   subTree: PropTypes.object.isRequired
